2229ce15496e9f213b7fd80b766a6b1444503ffa
skang1004 <saejin.kang1004@gmail.com>
2020-05-14 00:18:19 -0700

    working on mainjs features

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(request, sender, sendRequest) {
   const todos = request["todos"];
+  console.log("todos", todos);
   const timerValue = request["timerValue"];
   const body = document.querySelector("body");
   const currentDisplay = window.getComputedStyle(body).display;
@@ -11,17 +12,63 @@ function gotMessage(request, sender, sendRequest) {
   const timerInMS = timerValue * 60000;
 
   setTimeout(() => {
-    console.log("inside setTime");
-    body.style.transition = "opacity 3s";
+    body.style.transition = "opacity 5s";
     body.style.opacity = "0";
+    setTimeout(() => {
+      // after display is = 'none'
+      body.style.display = "none";
+      const html = document.querySelector("html");
+      const ul = document.createElement("ul");
+      ul.setAttribute("class", "ul");
+      ul.style.marginTop = "20rem";
+      ul.style.textAlign = "center";
+      ul.style.fontSize = "4.5rem";
+      ul.style.fontFamily = "Arial, Helvetica, sans-serif";
+
+      for (let i = 0; i < todos.length; i++) {
+        const li = document.createElement("li");
+        li.innerText = todos[i];
+        li.setAttribute("id", `${i}`);
+        li.style.listStyle = "none";
+        li.style.fontFamily = "Arial, Helvetica, sans-serif";
+        ul.appendChild(li);
+        li.addEventListener("click", (event) => {
+          const currentID = event.target.id;
+          const currentItem = document.getElementById(currentID);
+          //   const currentItemText = currentItem.innerText;
+          if (
+            window.getComputedStyle(currentItem).textDecoration !==
+            "line-through"
+          ) {
+            currentItem.style.textDecoration = "line-through";
+          }
+        });
+      }
+      const button = document.createElement("button");
+      button.innerText = "Back to Browsing!";
+
+      //troubleshooting bringing back the display on button 'bring back display'
+      button.addEventListener("click", (event) => {
+        html.removeChild("ul");
+        body.style.display = currentDisplay;
+        body.style.opacity = currentOpacity;
+        setTimeout(() => {
+          body.style.transition = currentTransition;
+        }, 3000);
+      });
+      ul.appendChild(button);
+      html.prepend(ul);
+
+      //grab the html element in html file
+      //prepend our changes to that html element
+      // add radio button or something for each element that checks if user finished task
+      // add another button that grabs checked elements and deletes from the storage
+      // onclick rerender the browser dom and also delete the stuff that was checked off
+      // if list is empty, then reward the user with some congratulations or smth idk
+
+      // also fade in our stuff
+    }, 5000);
     //fade the display out using opacity
-    // after display is = 'none'
-    //grab the html element in html file
-    //prepend our changes to that html element
-    // add radio button or something for each element that checks if user finished task
-    // add another button that grabs checked elements and deletes from the storage
-    // onclick rerender the browser dom and also delete the stuff that was checked off
-    // if list is empty, then reward the user with some congratulations or smth idk
   }, timerInMS);
   /**
    *
