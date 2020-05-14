c1a22389cf5427fbe11af185fdca5c6b178e12a3
skang1004 <saejin.kang1004@gmail.com>
2020-05-14 11:20:44 -0700

    working on styling a bit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const todosList = [];
 const addTodoButton = document.getElementById("add_todo");
+
+const taskInput = document.getElementById("text");
+taskInput.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    const input = document.querySelector("input");
+    const listItem = document.createElement("li");
+    listItem.innerText = input.value;
+    todosList.push(input.value);
+    input.value = "";
+    const ul = document.querySelector("#todos");
+    ul.appendChild(listItem);
+  }
+});
+
 addTodoButton.addEventListener("click", () => {
   const input = document.querySelector("input");
   const listItem = document.createElement("li");
@@ -12,6 +26,20 @@ addTodoButton.addEventListener("click", () => {
 
 // const timerValue = timer.value || 5000;
 
+const timerInMinutes = document.getElementById("number");
+timerInMinutes.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    const timer = timerInMinutes.value || 0.04;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        todos: todosList,
+        timerValue: timer,
+      });
+    });
+    window.close();
+  }
+});
+
 const confirmList = document.querySelector("#confirm_list");
 confirmList.addEventListener("click", () => {
   console.log("clicked");
@@ -23,4 +51,5 @@ confirmList.addEventListener("click", () => {
       timerValue: timerValue,
     });
   });
+  window.close();
 });
